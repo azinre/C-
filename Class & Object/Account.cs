8ab57ed0094c8e036a477bc1f010f0ba484e0899
Azin Rezaeian <66428866+azinre@users.noreply.github.com>
2024-01-24 07:56:40 -0800

    Add files via upload

---
+++ Class & Object/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4._1
+{
+    internal class Account
+    {
+        public int AccountNumber { get; set; }
+        public string OwnerName { get; set; }
+        public double Balance { get; set; }
+        public double MonthlyDepositAmount { get; set; }
+        public static double MonthlyFee { get; } = 4.0;
+        public static double MonthlyInterestRate { get; } = 0.0025;
+        public static double MinimumInitialBalance { get; } = 1000;
+        public static double MinimumMonthlyDeposit { get; } = 50;
+
+        public Account(string ownerName, double initialDeposit)
+        {
+            this.AccountNumber = new Random().Next(90000, 100000);
+            this.OwnerName = ownerName;
+            this.Balance = Math.Round(initialDeposit, 2);
+        }
+
+        public void Deposit(double amount)
+        {
+            this.Balance += amount;
+            this.Balance = Math.Round(this.Balance, 2); 
+        }
+
+        public void Withdraw(double amount)
+        {
+            this.Balance -= amount;
+            this.Balance = Math.Round(this.Balance, 2);
+        }
+    }
+}
---
+++ Class & Object/Bank.cs
@@ -0,0 +1,57 @@
+﻿using Lab4._1;
+using System.Xml.Linq;
+class Bank
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of months to deposit: ");
+        int months = Convert.ToInt32(Console.ReadLine());
+
+        var accounts = new List<Account>();
+
+        while (true)
+        {
+            Console.WriteLine("Enter customer's name: ");
+            string name = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                break;
+            }
+
+            Console.WriteLine("Enter " + name + "'s initial deposit amount (minimum  $1,000.00): ");
+            double initialDeposit = Convert.ToDouble(Console.ReadLine());
+            while (initialDeposit < Account.MinimumInitialBalance)
+            {
+                Console.WriteLine($"The minimum initial deposit amount is {Account.MinimumInitialBalance}. Enter again:");
+                initialDeposit = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter " + name + "'s monthly deposit amount (minimum $50.00): ");
+            double monthlyDeposit = Convert.ToDouble(Console.ReadLine());
+            while (monthlyDeposit < Account.MinimumMonthlyDeposit)
+            {
+                Console.WriteLine($"The minimum monthly deposit amount is {Account.MinimumMonthlyDeposit}. Enter again:");
+                monthlyDeposit = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine("");
+
+            var account = new Account(name, initialDeposit);
+            account.MonthlyDepositAmount = monthlyDeposit;
+            accounts.Add(account);
+        }
+        foreach (var account in accounts)
+        {
+            for (int i = 0; i < months; i++)
+            {
+                account.Withdraw(Account.MonthlyFee);
+                account.Deposit(account.Balance * Account.MonthlyInterestRate);
+                account.Deposit(account.MonthlyDepositAmount);
+            }
+            Console.WriteLine(" After " + months + " month," + account.OwnerName + "'s account (#" + account.AccountNumber + "), has a balance of : "+ account.Balance.ToString("C"));
+
+        }
+        Console.WriteLine("Press Enter To Compelete");
+    }
+   
+}
+
